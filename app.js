d07338809480d4a453eda518f19ed203c4036d64
Matthew Landeros Guillen <114602530+mattcodesplusdesign@users.noreply.github.com>
2022-11-15 22:10:57 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar-logo');
+const dropMenu = document.getElementById('drop-menu-src')
+const dropList = document.getElementById('drop-list')
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// Display Links Drop Menu
+
+dropList.style.display = "none";
+
+dropMenu.addEventListener('mouseenter', (event) => {
+    if(dropList.style.display == "none"){
+       dropList.style.display = "block"
+    }
+else{
+    dropList.style.display= "none"
+}
+})
+
