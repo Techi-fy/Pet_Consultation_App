06bcddfdcd8846ff295fa1bb0f47e2fc198c1e01
Matthew Landeros Guillen <matthewlguillen@gmail.com>
2022-12-21 17:58:12 -0500

    fixed UI bugs with buttons

--- public/javascripts/functional.js
+++ public/javascripts/functional.js
@@ -5,6 +5,7 @@ const dropMenu = document.getElementById("drop-menu-src");
 const dropList = document.getElementById("drop-list");
 const dropSubMenu = document.getElementById("drop-sublist");
 const petCareLink = document.querySelector(".pet-carelink");
+const aboutSec = document.getElementById("about-sec")
 //document.getElementById('error')
 
 // Display Mobile Menu
@@ -36,10 +37,14 @@ dropList.addEventListener("mouseleave", hoverRemoved);
 function subListOnOpacity() {
   dropSubMenu.style.opacity = "1";
   dropSubMenu.style.zIndex = "99";
+  dropSubMenu.style.zIndex = "99";
+  aboutSec.style.visibility = "hidden";
 }
+
 function subListOffOpacity() {
   dropSubMenu.style.opacity = "0";
   dropSubMenu.style.zIndex = "-3";
+  aboutSec.style.visibility = "visible";
 }
 petCareLink.addEventListener("click", subListOnOpacity);
 menu.addEventListener("click", subListOffOpacity);
